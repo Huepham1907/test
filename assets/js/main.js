4aeea881dc5d5dbcb259c3f9f0722d21ea6120de
unknown <you@example.com>
2022-07-06 00:28:28 +0700

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+$(".owl-carousel").owlCarousel({
+  loop: false,
+  margin: 16,
+  nav: true,
+  dots: false,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 3,
+    },
+    1100: {
+      items: 6,
+    },
+  },
+});
